3ba040a384a90f31a838eeb10cdbaa6bd5ac0534
simkaywee <simkaywee@gmail.com>
2025-09-03 16:58:32 +0800

    Update models.js

--- backend/db/models.js
+++ backend/db/models.js
@@ -7,10 +7,12 @@ const productSchema = new mongoose.Schema({
     carbonEmissions: { type: Number, required: true },
     plasticUsage: { type: Number, required: true },
     points: { type: Number, default: 0 },
-    image: { 
-        data: Buffer,   // actual binary data
-        contentType: String // e.g. 'image/png', 'image/jpeg'
-    }
+    price: { type: Number, required: true },
+    category: { type: String, required: true },
+    sustainabilityScore: { type: Number, required: true }, // 0-100
+    barcode: { type: String, unique: true }, 
+    imageUrl: { type: String } // store S3 URL here
+    
 });
   
 const Product = mongoose.model('Product', productSchema, 'SuperMarket');
@@ -20,8 +22,12 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
   points: { type: Number, default: 0 },
-  receipts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Receipt' }]
+  monthlyRank: { type: Number, default: 0 },
+  neighbourhoodRank: { type: Number, default: 0 },
+  neighbourhood: { type: String },
+
 });
+
 
 const User = mongoose.model('User', userSchema);
 
